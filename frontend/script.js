03ea3ad13e6eedb02cb819cd7d1a85ce61d061c7
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-11-20 02:18:16 +0100

    i actually kinda like this

---
+++ frontend/script.js
@@ -0,0 +1,134 @@
+let model;
+
+let board = createBoard();
+
+const $bestMoves = $("#best-moves");
+
+async function loadModel() {
+  model = await tf.loadLayersModel("js_models/model.json");
+  return model;
+}
+
+loadModel().then(update);
+
+function createBoard(options = {}) {
+  return Chessboard("board", Object.assign(options, {
+    draggable: true,
+    dropOffBoard: "trash",
+    position: "start",
+    pieceTheme,
+    onDrop,
+    onSnapEnd,
+  }));
+}
+
+let game = new Chess();
+
+function pieceTheme(piece) {
+  return `assets/img/chesspieces/${settings.theme}/${piece}.svg`;
+}
+
+function onDrop(source, target) {
+  let move = game.move({
+    from: source,
+    to: target,
+    promotion: "q",
+  });
+
+  if (move === null) return "snapback";
+
+  update();
+}
+
+function getMove(input) {
+  let move = game.move(input);
+  if (move != null) game.undo();
+  return move;
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+function update() {
+  if (model == null) return;
+
+  const input = posToInput(game.fen());
+  const output = model.predict(input).dataSync();
+  let amount = 10;
+  let moves = [];
+  while (moves.length < 8 && amount < 10001) {
+    moves = outputToMoves(output, { amount })
+      .map((obj) => Object.assign(obj, { move: getMove(obj) }))
+      .map((obj) => Object.assign(obj, { valid: obj.move != null }))
+      .filter(({ valid }) => settings.showInvalidMoves || valid);
+    amount *= 10;
+  }
+
+  if (
+    (game.turn() === "w" && settings.playAuto.white) ||
+    (game.turn() === "b" && settings.playAuto.black)
+  ) {
+    if (
+      game.in_threefold_repetition() ||
+      game.in_checkmate() ||
+      game.in_stalemate()
+    ) {
+      settings.playAuto = { white: false, black: false };
+      return;
+    }
+    game.move(moves[0]);
+    board.position(game.fen());
+    requestAnimationFrame(update);
+  }
+
+  updateBestMoves(moves);
+}
+
+function updateBestMoves(moves, amount = 8) {
+  $bestMoves.empty();
+
+  moves.slice(0, amount).forEach(({ act, from, to, move, valid }) => {
+    $bestMoves.append(
+      $("<button>")
+        .addClass("move")
+        .addClass(valid ? "valid" : "invalid")
+        .css("background-color", valid ? actToColor(act) : "darkgray")
+        .click(() => {
+          game.move({ from, to });
+          board.position(game.fen());
+          update();
+        })
+        .append(
+          $("<span>")
+            .addClass("act")
+            .text((act * 100).toFixed(2)),
+          $("<span>")
+            .addClass("move")
+            .html(move ? moveToHtml(move) : `${from}-${to}`)
+        ),
+      $("<br>")
+    );
+  });
+}
+
+const interpolate = (t) => (x) =>
+  1 + 1 / t - 1 / (t * x + 1) - 1 / (t * t * x + t);
+
+function actToColor(act) {
+  // Return a color based on the activation value.
+  // The color is a gradient from red to green.
+  // It is red when the activation is 0 and green when the activation is 1.
+  // The gradient is logarithmic.
+  let newAct = interpolate(25)(act);
+  let red = Math.min(1.0, 2.0 - 2.0 * newAct) * 255;
+  let green = Math.min(1.0, 2.0 * newAct) * 255;
+  return `rgb(${red}, ${green}, 0)`;
+}
+
+function moveToHtml(move) {
+  let san = move.san;
+  if (move.piece !== "p" && !san.startsWith("O")) san = san.slice(1);
+  const imgSrc = pieceTheme(move.color + move.piece.toUpperCase());
+  return `<div class="move-desc"><img class="piece-letter" src="${imgSrc}"/> <span>${san}</span></div>`;
+}
---
+++ frontend/settings.js
@@ -0,0 +1,103 @@
+const settings = {
+  showInvalidMoves: false,
+  playAuto: {
+    white: false,
+    black: false,
+  },
+  theme: "gioco",
+};
+
+const $settings = $("#settings");
+
+$settings.append($("<h2>").text("Settings"));
+
+$settings.append(
+  $("<label>")
+    .text("Show invalid moves")
+    .append(
+      $("<input>")
+        .attr("type", "checkbox")
+        .change(function () {
+          settings.showInvalidMoves = $(this).prop("checked");
+          update();
+        })
+        .prop("checked", settings.showInvalidMoves)
+    )
+);
+
+$settings.append($("<span>").text("Play automatically for: "));
+
+for (const color of ["White", "Black"]) {
+  $settings.append(
+    $("<label>")
+      .html(`&emsp;${color}`)
+      .append(
+        $("<input>")
+          .attr("type", "checkbox")
+          .change(function (elt) {
+            settings.playAuto[color.toLowerCase()] = $(this).prop("checked");
+            update();
+          })
+          .prop("checked", settings.playAuto[color.toLowerCase()])
+      )
+  );
+}
+
+const licenses = {
+  ByNcSa: "CC-BY-NC-SA 4.0", // + '<img src=https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/>',
+  BySa: "CC-BY-SA 4.0", // + <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"/>',
+  Mit: "MIT",
+};
+const themes = {
+  horsey: { by: "cham and michael1241", license: licenses.ByNcSa },
+  shapes: { by: "flugsio", license: licenses.BySa },
+  maestro: { by: "sadsnake1", license: licenses.ByNcSa },
+  fresca: { by: "sadsnake1", license: licenses.ByNcSa },
+  cardinal: { by: "sadsnake1", license: licenses.ByNcSa },
+  icpieces: { by: "sadsnake1", license: licenses.ByNcSa },
+  gioco: { by: "sadsnake1", license: licenses.ByNcSa },
+  tatiana: { by: "sadsnake1", license: licenses.ByNcSa },
+  staunty: { by: "sadsnake1", license: licenses.ByNcSa },
+  dubrovny: { by: "sadsnake1", license: licenses.ByNcSa },
+  libra: { by: "sadsnake1", license: licenses.ByNcSa },
+  anarcandy: { by: "caderek", license: licenses.ByNcSa },
+  disguised: { by: "danegraphics", license: licenses.ByNcSa },
+  chess7: { by: "Style-7", license: null },
+  companion: { by: "David L. Brown", license: null },
+  fantasy: { by: "Maurizio Monge", license: null },
+  spatial: { by: "Maurizio Monge", license: null },
+  leipzig: { by: "Armando Hernandez Marroquin", license: null },
+};
+
+$settings.append(
+  // Selection of piece icons
+  $("<label>")
+    .text("Piece icons")
+    .append(
+      $("<select>")
+        .val(settings.theme)
+        .change(function () {
+          settings.theme = $(this).val();
+          const obj = themes[settings.theme];
+          $credit.html(
+            `by ${obj.by} ${obj.license ? "under " + obj.license : ""}`
+          );
+          board.destroy();
+          board = createBoard();
+          update();
+          board.position(game.fen());
+        })
+        .append(
+          (() => {
+            let keys = Object.keys(themes);
+            keys.sort();
+            return keys;
+          })().map((theme) => {
+            return $("<option>").attr("value", theme).text(theme);
+          })
+        )
+    )
+);
+
+const $credit = $("<div>").attr("id", "credit");
+$settings.append($credit);
