8212b7a5bddd3a172c7c4904d6d51b5d07609fe2
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-11-21 16:56:54 +0100

    I have a chessboard.

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
-    <h1>My App</h1>
-    <p>{{ message }}</p>
+    <div id="board"></div>
   </div>
 </template>
 
 <script lang="ts">
+import type { Model } from '@/neural-models/model'
+import type { FromToOutput, StandardPositionalInput } from '@/neural-models/types'
+
 import { loadFromToOutputModel } from '@/neural-models/model'
 import { fromToOutputToMoves } from '@/neural-models/chess_conversions'
+
 export default {
-  async setup() {
-    const model = await loadFromToOutputModel();
+  created() {
+    loadFromToOutputModel().then(m => this.model = m)
+  },
+  mounted() {
+    // @ts-ignore
+    window.board = new Chessboard('#board', {
+      draggable: true,
+      dropOffBoard: "trash",
+      position: "start",
+      pieceTheme: this.pieceTheme,
+      // onDrop,
+      // onSnapEnd,
+    })
+  },
+  methods: {
+    pieceTheme(piece: string) {
+      const theme = "maestro";
+      return `assets/img/chesspieces/${theme}/${piece}.svg`;
+    }
   },
   data: () => ({
     message: 'Hello World!',
+    model: null as Model<StandardPositionalInput, FromToOutput> | null
   }),
 }
 </script>
