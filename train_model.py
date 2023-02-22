bac362a5827c85bf450107a0075eb52331d5dba7
leo <leonardo.blume@gymnasium-essen-werden.de>
2023-02-22 17:35:57 +0100

    Add CLI arguments (for future auto training)

--- train_model.py
+++ train_model.py
@@ -4,6 +4,8 @@ import os
 import numpy as np
 import psutil
 
+import argparse
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 # from filenames import MODEL_INPUT, MODEL_NAME, MODEL_OUTPUT
@@ -11,23 +13,45 @@ from tensorflow.keras import layers, models
 
 print("TensorFlow version: ", tf.__version__)
 
-MODEL_NAME = "models/15Mtrain-1024-4layers-mae.h5"
+parser = argparse.ArgumentParser(
+    description="Train a neural model using data created in Rust.",
+)
+parser.add_argument(
+    "--output", type=str, help="The name of the model to be saved.", required=True
+)
+parser.add_argument(
+    "--input", type=str, help="The name of the input directory to be used.", required=True
+)
+parser.add_argument(
+    "--layers", type=int, help="The number of layers in the neural network.", default=4
+)
+parser.add_argument(
+    "--neurons", type=int, help="The number of neurons in each layer.", default=512
+)
+parser.add_argument(
+    "--batch", type=int, help="The batch size to be used."
+)
+parser.add_argument(
+    "--tensorboard", action="store_true", help="Whether to use tensorboard."
+)
 
-GRAPH_NAME = f"figures/{MODEL_NAME.split('/')[-1].split('.')[0]}-graph.png"
+args = parser.parse_args()
+
+MODEL_NAME = f"models/{args.output}.h5"
+GRAPH_NAME = f"figures/{args.output}.png"
 
 if os.path.isfile(MODEL_NAME):
     print("Model already exists. Exiting.")
     exit(1)
 
-MODEL_INPUT = "npy_files/20M_neural_input/{file}"
-MODEL_OUTPUT = "npy_files/20M_neural_output/{file}"
+MODEL_INPUT = "npy_files/" + args.input + "_input/{file}"
+MODEL_OUTPUT = "npy_files/" + args.input + "_output/{file}"
 
-TENSORBOARD = True
+TENSORBOARD = args.tensorboard
 
-TOTAL_DATA_SIZE = 20_000_000
-AMOUNT_OF_FILES = 40
-
-DATA_PER_FILE = TOTAL_DATA_SIZE // AMOUNT_OF_FILES
+AMOUNT_OF_FILES = os.listdir(f"npy_files/{args.input}_input").__len__()
+DATA_PER_FILE = len(np.load(MODEL_INPUT.format(file="0.npy")))
+TOTAL_DATA_SIZE = AMOUNT_OF_FILES * DATA_PER_FILE
 
 assert TOTAL_DATA_SIZE % AMOUNT_OF_FILES == 0
 
@@ -37,7 +61,7 @@ VALIDATION_FILES = [ f"{i}.npy" for i in range(0, AMOUNT_OF_FILES) if (i + 7) % 
 assert len(TRAINING_FILES) + len(VALIDATION_FILES) == AMOUNT_OF_FILES
 assert len(set(TRAINING_FILES) & set(VALIDATION_FILES)) == 0
 
-BATCH_SIZE = 2048
+BATCH_SIZE = args.batch
 EPOCHS = 128
 
 TRAINING_STEPS = len(TRAINING_FILES) * DATA_PER_FILE // BATCH_SIZE // EPOCHS
@@ -85,9 +109,10 @@ validation_generator = generator_generator(VALIDATION_FILES)
 
 model = models.Sequential()
 model.add(layers.Dense(512, activation='relu', input_shape=(1 + (1+2*6) * 64,)))
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(512, activation='relu'))
+
+for i in range(args.layers - 1):
+    model.add(layers.Dense(args.neurons, activation='relu'))
+
 model.add(layers.Dense(4096, activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
